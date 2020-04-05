1c67b66c77c2b22a5ee2f11f166d9419b5669b1a
Andrew Krumm <aekrumm@msla0574.local>
2020-04-05 09:43:28 -0400

    ggployl

--- app.R
+++ app.R
@@ -1,23 +1,23 @@
 
+# Load packages
+
 library(shiny)
 library(tidyverse)
 library(shinyjs)
 library(plotly)
+library(readxl)
+
+# Start UI
 
 ui <- fluidPage(
     
     titlePanel("Projected Staffing Demand"),
     
     h4("Inputs: CHIME-based projections and staffing ratios"),
-    p("Before using this tool, be sure to work through the CHIME application. Once you've completed your projections,
-      download and modify the staffing ratios. You can uses this tool, to run simulations <>."),
+    ("This application only works with two specific file types. The first is the"), strong("projected census"), ("file that you downloaded from CHIME (e.g. 2020-04-02_projected_census.csv)"),
+    ("The second the"), strong("staffing tables"), ("file (e.g., staff_tabel.xlsx), which you can download from above."),
     
-    strong("Plot 1:"), ("ICU Normal,"), strong("Plot 2:"), ("ICU Crisis,"), 
-    strong("Plot 3:"), ("Non-ICU," ), strong("Plot 4:"), ("Non-ICU Crisis,"),
-    
-    #textOutput("b"),
-    
-    plotlyOutput("plotY"),
+    plotlyOutput("plot"),
     
     fluidRow(
         column(3,
@@ -31,11 +31,9 @@ ui <- fluidPage(
                
                hr(),
                
-               fileInput("team_in", "Staffing Ratios (.csv)",
+               fileInput("team_in", "Staffing Ratios (.xlsx)",
                          multiple = FALSE,
-                         accept = c("text/csv",
-                                    "text/comma-separated-values,text/plain",
-                                    ".csv")),
+                         accept = c(".xlsx")),
                
                hr(),
                
@@ -48,16 +46,8 @@ ui <- fluidPage(
 # Server
 server <- function(input, output) {
 
-    #team <- input$team_in
-    
-    #team <- read_csv(
-    #    team$datapath,
-    #    col_names = T)    
-    
-chart_data <- function(chime) {
-    
-    #team <- read_rds("./data/team_ratio.rds") # pull from modifiable data
-    
+  chart_data <- function(chime) {
+
     # Clean up CHIME 
     chime <- chime %>%
         filter(day >= 0)
@@ -65,7 +55,7 @@ chart_data <- function(chime) {
     # Get rows after clean
     chime_row <- nrow(chime)
     
-    # Create four plots: ICU / General; Normal / Stretch
+    # Create four plots: ICU / General; Normal / Crisis
     
     icu_ratio <- data2() %>% 
         filter(team_type == "ICU") 
@@ -73,16 +63,16 @@ chart_data <- function(chime) {
     icu_norm <- icu_ratio %>%
         select(role, n_bed_per_person) %>%
         mutate(day = 0,
-               team_type = "1 ICU Normal") %>%
+               team_type = "ICU Normal") %>%
         pivot_wider(., id_cols = c("day", "team_type"), names_from = role, values_from = n_bed_per_person) %>%
         add_row(day = 1:chime_row) %>%
         fill(., 2:12)
     
-    icu_stretch <- icu_ratio %>%
-        select(role, n_bed_per_person_stretch) %>%
+    icu_crisis <- icu_ratio %>%
+        select(role, n_bed_per_person_crisis) %>%
         mutate(day = 0,
-               team_type = "2 ICU Stretch") %>%
-        pivot_wider(., id_cols = c("day", "team_type"), names_from = role, values_from = n_bed_per_person_stretch) %>%
+               team_type = "ICU Crisis") %>%
+        pivot_wider(., id_cols = c("day", "team_type"), names_from = role, values_from = n_bed_per_person_crisis) %>%
         add_row(day = 1:chime_row) %>%
         fill(., 2:12)
     
@@ -92,22 +82,22 @@ chart_data <- function(chime) {
     gen_norm <- gen_ratio %>%
         select(role, n_bed_per_person) %>%
         mutate(day = 0,
-               team_type = "3 General Normal") %>%
+               team_type = "General Normal") %>%
         pivot_wider(., id_cols = c("day", "team_type"), names_from = role, values_from = n_bed_per_person) %>%
         add_row(day = 1:chime_row) %>%
         fill(., 2:10)
     
-    gen_stretch <- gen_ratio %>%
-        select(role, n_bed_per_person_stretch) %>%
+    gen_crisis <- gen_ratio %>%
+        select(role, n_bed_per_person_crisis) %>%
         mutate(day = 0,
-               team_type = "4 General Stretch") %>%
-        pivot_wider(., id_cols = c("day", "team_type"), names_from = role, values_from = n_bed_per_person_stretch) %>%
+               team_type = "General Crisis") %>%
+        pivot_wider(., id_cols = c("day", "team_type"), names_from = role, values_from = n_bed_per_person_crisis) %>%
         add_row(day = 1:chime_row) %>%
         fill(., 2:10) 
     
     # Append tables
     
-    lookup <- bind_rows(icu_norm, icu_stretch, gen_norm, gen_stretch)
+    lookup <- bind_rows(icu_norm, icu_crisis, gen_norm, gen_crisis)
     
     lookup <- lookup %>%
         pivot_longer(., 3:14, names_to = "role", values_to = "n_bed_per_person")  %>%
@@ -121,11 +111,11 @@ chart_data <- function(chime) {
         mutate(projected_bed_per_person =  icu / n_bed_per_person )
     
     chime_lookup <- chime_lookup %>%
-        filter(!is.infinite(projected_bed_per_person)) %>%
-        group_by(team_type, day) %>%
-        mutate(rnk = rank(n_bed_per_person, ties.method = "first")) %>%
-        ungroup() %>%
-        mutate(ord_new = fct_reorder2(role, rnk, projected_bed_per_person))
+        filter(!is.infinite(projected_bed_per_person)) # %>%
+        #group_by(team_type, day) %>%
+        #mutate(rnk = rank(n_bed_per_person, ties.method = "first")) %>%
+        #ungroup() %>%
+        #mutate(ord_new = fct_reorder2(role, rnk, projected_bed_per_person))
 }
 
 data <- reactiveVal(NULL)
@@ -157,10 +147,14 @@ data2 <- reactiveVal(NULL)
         
         enable("generateButton")
         
-        tmpdf2 <- read_csv(
-            inFile$datapath,
-            col_names = T,
-            local = locale(encoding = "latin1"))
+        #tmpdf2 <- read_csv(
+        #    inFile$datapath,
+        #    col_names = T,
+        #    local = locale(encoding = "latin1"))
+        
+        
+        tmpdf2 <- read_xlsx(
+            inFile$datapath)
         
         data2(tmpdf2)
         
@@ -169,9 +163,9 @@ data2 <- reactiveVal(NULL)
     display <- eventReactive(input$generateButton, 
                                chart_data(data())) 
     
-    output$plot <- renderPlot({
+    output$plot <- renderPlotly({
         
-    ggplot(display(), aes(x = date, y = projected_bed_per_person, colour = ord_new, group = role)) +
+    p <-  ggplot(display(), aes(x = date, y = projected_bed_per_person, colour = role, group = role)) +
         geom_line() +
         labs(title = "Projected Staffing Needs",
              x = "Date",
@@ -179,32 +173,13 @@ data2 <- reactiveVal(NULL)
              colour = "Roles",
              caption = "Estimates from CHIME and user-inputted ratios") +
         theme_bw() +
-        facet_wrap(~ team_type, scales = "free")
+        facet_wrap(~ team_type, scales = "free", ncol = 4)
+    
+    fig <- ggplotly(p)
+    
+    fig
     
     })    
-        
-    output$plotY <- renderPlotly({
-        
-        display() %>% 
-            split(display()$team_type) %>% 
-            lapply(function(x) {
-                plot_ly(data = x, 
-                        x = ~date, 
-                        y = ~projected_bed_per_person, 
-                        type= "scatter", 
-                        mode = 'lines',
-                        color=~ord_new)
-            }) %>% 
-            
-            subplot(margin = .05)
-
-        })
-        
-  #  output$b <- renderText({
-  #      unique(icu_norm$team_type)
-  #  })
-    
 }
 
-# Create Shiny app ----
 shinyApp(ui, server)
