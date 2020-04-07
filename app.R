949c571b94615b504490a466f626b5c5eb32dc7c
Xilin Chen <xilinc@med.umich.edu>
2020-04-07 18:03:40 -0400

    default plots

--- app.R
+++ app.R
@@ -12,6 +12,16 @@ library(readxl)
 chime <- read_csv("./data/2020-04-02_projected_census.csv")
 team <- read_xlsx("./data/staff_table.xlsx")
 
+
+team_icu_def = team %>%
+  filter(team_type == "ICU") %>%
+  transmute(role, ratio = n_bed_per_person, ratio_s = n_bed_per_person_crisis)
+
+team_gen_def = team %>%
+  filter(team_type == "General") %>%
+  transmute(role, ratio = n_bed_per_person, ratio_s = n_bed_per_person_crisis)
+
+
 # data procssing for plot
 source("function/chart_data.R")
 
@@ -54,90 +64,113 @@ ui <- fluidPage(
         accept = c(".xlsx")
       ),
       #hr(),
-      actionButton("generateButton", label = "Generate Plot"),
+      # actionButton("generateButton", label = "Generate Plot"),
       
       hr(),
       
       downloadButton("downloadData", "Download Combined File")
     ),
     
-    mainPanel(tabsetPanel(
-      type = "tabs",
-      tabPanel("Crisis", plotlyOutput("plot_crisis")),
-      tabPanel("Normal", plotlyOutput("plot_norm")),
+    mainPanel(
       
-      # editable tables -------
-      tabPanel(
-        value = "edit_ratio_table",
-        title = "Patient-to-Staff Ratios",
-        br(),
+      
+      tags$style(HTML("
+        .tabbable > .nav > li[class=active]    > a[data-value='Normal'] {background-color: #9dc183; color:black}
+        .tabbable > .nav > li[class=active]    > a[data-value='Crisis'] {background-color: #8D021F; color:white}
+  ")),
+      
+      tabsetPanel(
+        id = "inTabset",
         
-        helpText(
-          strong("Important note:"),
-          "These estimates are designed to give a sense of general staffing needs, but your needs may vary based on local conditions."
-        ),
+        type = "tabs",
         
-        
-        actionButton("reset", "Clear Table", icon("table"),
-                     style = "color: #fff; background-color: #228B22; border-color: #2e6da4"),
-        actionButton("reset_to_ori", "Reset to Default", icon("undo"),
-                     style = "color: #fff; background-color: #228B22; border-color: #2e6da4"),
+        # plot tabs
+        tabPanel("Normal", plotlyOutput("plot_norm")),
+        tabPanel("Crisis", plotlyOutput("plot_crisis")),
         
         
-        br(),
-        br(),
-        
-        p(
-          strong("Right click"),
-          "in a cell to add and delete row;",
-          "select cell and type the new value",
-          style = "font-size:16px"
-        ),
-        
-        h4("ICU"),
-        
-        div(rHandsontableOutput("x1"), style = "font-size: 120%"),
-        
-        br(),
-        
-        h4("Non-ICU"),
-        
-        div(rHandsontableOutput("x2"), style = "font-size: 120%"),
-        
-        br(),
-        
-        
-        downloadButton("downloadData_all_ratio", "Download Staffing Ratios",
-                       style = "color: #fff; background-color: #228B22; border-color: #2e6da4"),
-        
-        
-        column(
-          12,
+        # editable tables -------
+        tabPanel(
+          value = "edit_ratio_table",
+          title = "Patient-to-Staff Ratios",
           br(),
-          tags$div(
-            "Role: List of possible staff roles",
-            tags$br(),
-            "Ratio (normal) = the patient:staff ratio (i.e. how many patients each staff member cares for)",
-            tags$br(),
-            "Ratio (Crisis Mode) = the patient:staff ratio during a ‘crisis mode’ (ie. the maximum number patients each staff member can care for)",
-            tags$br(),
-            tags$br(),
-            tags$br(),
-            "* Default patient-to-staff ratios are based on real staffing ratios at a collaborating academic medical center that has undertaken extensive emergency preparedness work for this pandemic.",
+          
+          helpText(
+            strong("Important note:"),
+            "These estimates are designed to give a sense of general staffing needs, but your needs may vary based on local conditions."
+          ),
+          
+          
+          actionButton("reset", "Clear Table", icon("table"),
+                       style = "color: #fff; background-color: #228B22; border-color: #2e6da4"),
+          actionButton("reset_to_ori", "Reset to Default", icon("undo"),
+                       style = "color: #fff; background-color: #228B22; border-color: #2e6da4"),
+          br(),
+          br(),
+          actionButton(
+            "generateButton",
+            label = "Generate Plot",
+            icon("chart-line"),
+            style = "color: #fff; background-color: #228B22; border-color: #2e6da4"
+          ),
+          
+          
+          
+          
+          br(),
+          br(),
+          
+          p(
+            strong("Right click"),
+            "in a cell to add and delete row;",
+            "select cell and type the new value",
+            style = "font-size:16px"
+          ),
+          
+          h4("ICU"),
+          
+          div(rHandsontableOutput("x1"), style = "font-size: 120%"),
+          
+          br(),
+          
+          h4("Non-ICU"),
+          
+          div(rHandsontableOutput("x2"), style = "font-size: 120%"),
+          
+          br(),
+          
+          
+          downloadButton("downloadData_all_ratio", "Download Staffing Ratios",
+                         style = "color: #fff; background-color: #228B22; border-color: #2e6da4"),
+          
+          
+          column(
+            12,
             br(),
-            br()
-          )
-        ),
-      )
-      
-    ))
+            tags$div(
+              "Role: List of possible staff roles",
+              tags$br(),
+              "Ratio (normal) = the patient:staff ratio (i.e. how many patients each staff member cares for)",
+              tags$br(),
+              "Ratio (Crisis Mode) = the patient:staff ratio during a ‘crisis mode’ (ie. the maximum number patients each staff member can care for)",
+              tags$br(),
+              tags$br(),
+              tags$br(),
+              "* Default patient-to-staff ratios are based on real staffing ratios at a collaborating academic medical center that has undertaken extensive emergency preparedness work for this pandemic.",
+              br(),
+              br()
+            )
+          ),
+        )
+        
+      ))
   )
 )
 
 
 
 # Server ---------------------
-server <- function(input, output) {
+server <- function(input, output, session) {
   
   # default team ratios vs. upload --------
   # Team ratio ---
@@ -177,6 +210,70 @@ server <- function(input, output) {
 
   # editable tables -------
     values <- reactiveValues()
+  
+  # reset reference table -------
+  reset_table = tibble(role = c("Role1", "Role2", "Role3"),
+                       ratio = as.numeric(rep(0, 3)),
+                       ratio_s = as.numeric(rep(0, 3)))
+  
+  # reset to clear table
+  observeEvent(input$reset,{
+    output$x1 <- renderRHandsontable({
+      rhandsontable(
+        reset_table %>%
+          rename(
+            "Ratio (Normal)" = ratio,
+            "Ratio (Crisis Mode)" = ratio_s,
+            Role = role
+          ) %>%
+          mutate_if(is.numeric, as.integer), rowHeaders = FALSE, width = 570, stretchH = "all"
+      ) %>% 
+        hot_cols(colWidths = 100) 
+    })
+    
+    output$x2 <- renderRHandsontable({
+      rhandsontable(
+        reset_table %>% 
+          rename(
+            "Ratio (Normal)" = ratio,
+            "Ratio (Crisis Mode)" = ratio_s,
+            Role = role
+          ) %>%
+          mutate_if(is.numeric, as.integer), rowHeaders = FALSE, width = 570, stretchH = "all"
+      ) %>% 
+        hot_cols(colWidths = 100) 
+    })
+  })
+  
+  # reset to default --------
+  observeEvent(input$reset_to_ori,{
+    output$x1 <- renderRHandsontable({
+      rhandsontable(
+        team_icu_def %>%
+          rename(
+            "Ratio (Normal)" = ratio,
+            "Ratio (Crisis Mode)" = ratio_s,
+            Role = role
+          ) %>%
+          mutate_if(is.numeric, as.integer), rowHeaders = FALSE, width = 570, stretchH = "all"
+      ) %>%
+        hot_cols(colWidths = 100)
+    })
+
+    output$x2 <- renderRHandsontable({
+      rhandsontable(
+        team_gen_def %>%
+          rename(
+            "Ratio (Normal)" = ratio,
+            "Ratio (Crisis Mode)" = ratio_s,
+            Role = role
+          ) %>%
+          mutate_if(is.numeric, as.integer), rowHeaders = FALSE, width = 570, stretchH = "all"
+      ) %>%
+        hot_cols(colWidths = 100)
+    })
+  })
+
     
     output$x1 <- renderRHandsontable({
       rhandsontable(
@@ -210,26 +307,53 @@ server <- function(input, output) {
     
     # calculations happen here ------
     
-    display_table <- reactive({
-      values$df = hot_to_r(input$x1) %>% 
-        mutate(team_type = "General") %>% 
-        rename(role = Role,
-               n_bed_per_person = "Ratio (Normal)" ,
-               n_bed_per_person_crisis = "Ratio (Crisis Mode)") %>% 
-        select(team_type, everything())
+    icu_ratio_table <- reactive({
+      if(is.null(input$x1))
+        return(
+          team_icu_def %>%
+            mutate(team_type = "ICU") %>% 
+            rename(n_bed_per_person = ratio ,
+                   n_bed_per_person_crisis = ratio_s) %>% 
+            select(team_type, everything())
+        )
       
-      values$df_gen = hot_to_r(input$x2) %>% 
+      values$df = hot_to_r(input$x1) %>% 
         mutate(team_type = "ICU") %>% 
         rename(role = Role,
                n_bed_per_person = "Ratio (Normal)" ,
                n_bed_per_person_crisis = "Ratio (Crisis Mode)") %>% 
         select(team_type, everything())
+    })
+    
+    gen_ratio_table <- reactive({
+      if(is.null(input$x2))
+        return(
+          team_gen_def %>%
+            mutate(team_type = "General") %>% 
+            rename(n_bed_per_person = ratio ,
+                   n_bed_per_person_crisis = ratio_s) %>% 
+            select(team_type, everything())
+        )
       
-      rbind(values$df, values$df_gen) %>% 
+      values$df = hot_to_r(input$x2) %>% 
+        mutate(team_type = "General") %>% 
+        rename(role = Role,
+               n_bed_per_person = "Ratio (Normal)" ,
+               n_bed_per_person_crisis = "Ratio (Crisis Mode)") %>% 
+        select(team_type, everything())
+    })
+      
+      
+    display_table <- reactive({
+      rbind(gen_ratio_table(), icu_ratio_table()) %>% 
         chart_data(chime = chime_table())
     })
     
     # plots -------
+    
+    observeEvent(input$generateButton, {
+      updateTabsetPanel(session, "inTabset",selected = "Normal")
+    })
     
     output$plot_crisis <- renderPlotly({
       p <-  ggplot(display_table() %>%
